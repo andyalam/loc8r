aefaaf45f9cbdf463b01466d4b134ecbc6cf521d
andyalam <andyalam@hotmail.com>
2016-01-29 19:42:22 -0800

    ch4 part 1

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var cntrlMain = require('../controllers/main');
+var cntrlLocations = require('../controllers/locations');
+var cntrlOthers = require('../controllers/others');
+
 
 /* GET home page. */
-router.get('/', cntrlMain.index);
+router.get('/', cntrlLocations.homeList);
+router.get('/location', cntrlLocations.locationInfo);
+router.get('/location/review/new', cntrlLocations.addReview);
 
 module.exports = router;
