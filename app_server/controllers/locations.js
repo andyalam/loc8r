535d7d699b260de338fba92284d183d6335af5e6
andyalam <andyalam@hotmail.com>
2016-02-02 23:54:32 -0800

    end of ch4

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,79 @@
 module.exports.homeList = function(req, res) {
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', { 
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 0,
+            facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+            distance: '100m'
+        },{
+            name: 'Dennys',
+            address: '395 Mudkip Lane, Palette, PK9 1PK',
+            rating: 3,
+            facilities: ['Food', '24/7', 'Diner style'],
+            distance: '200m'
+        },{
+            name: 'Gold\'s Gym',
+            address: '501 Swole Ln, Valhalla, LF9 PR2',
+            rating: 5,
+            facilities: ['Free weights', 'Squat racks', 'Sauna'],
+            distance: '1000m'
+        }]
+    });
 }
 
 module.exports.locationInfo = function (req, res) {
-    res.render('location-info', { title: 'Location Info' });
+    res.render('location-info', { 
+        title: 'Starcups',    
+        pageHeader: { title: 'Starcups' },
+        sidebar: {
+          context: 'is on the Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+          callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'  
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 0,
+            facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+            coords: { lat: 51.455041, lng: -0.9690884 },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Andy Alameddine',
+                rating: 5,
+                timestamp: '30 January, 2016',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            }, {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '25 February, 2016',
+                reviewText: 'It was okay. Coffee wasn\'t great, wifi was fast.'
+            }]
+        }
+    });
 }
 
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', { title: 'Add review' });
+    res.render('location-review-form', { 
+        title: 'Review Starcups on Loc8r', 
+        pageHeader: { title: 'Review Starcups' }
+    });
 }
