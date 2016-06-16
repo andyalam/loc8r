bee2561a0853ca9a688569f87ef785473ed4c8ad
andy <andyalam@hotmail.com>
2016-06-16 09:23:54 -0700

    post request

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -69,7 +69,30 @@ module.exports.locationsListByDistance = function(req, res, next) {
 
 // POST 
 module.exports.locationsCreate = function(req, res, next) {
-  
+    Loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [ parseFloat(req.body.lng), parseFloat(req.body.lat) ],
+    openingTimes: [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1,
+    }, {
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2,
+    }]
+  }, function(err, location) {
+    if (err) {
+      sendJsonResponse(res, 400, err);
+    } else {
+      console.log(location);
+      sendJsonResponse(res, 201, location);
+    }
+  });
 };
 
 // GET
