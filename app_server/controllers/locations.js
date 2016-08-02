81af46fa196c80916b08f78777b4e0368552f5d7
Andy  Alameddine <andya@bixly.com>
2016-08-02 13:27:38 -0700

    review form validition on model, client js, and backend

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -50,7 +50,8 @@ var renderDetailPage = function (req, res, locDetail) {
 var renderReviewForm = function(req, res, locDetail) {
   res.render('location-review-form', {
     title: 'Review ' + locDetail.name + ' on Loc8r',
-    pageHeader: { title: 'Review ' + locDetail.name }
+    pageHeader: { title: 'Review ' + locDetail.name },
+    error: req.query.err
   });
 };
 
@@ -172,15 +173,21 @@ module.exports.doAddReview = function(req, res, next) {
     json: postdata
   };
 
-  request(
-    requestOptions,
-    function(err, response, body) {
-      if (response.statusCode === 201) {
-        res.redirect('/location/'+ locationid);
-      } else {
-        _showError(req, res, response.statusCode);
+  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+    res.redirect('/location/' + locationid + '/review/new?err=val');
+  } else {
+    request(
+      requestOptions,
+      function(err, response, body) {
+        if (response.statusCode === 201) {
+          res.redirect('/location/' + locationid);
+        } else if (response.statusCode === 400 && body.name && body.name === "ValidationError") {
+          res.redirect('/location/' + locationid + '/review/new?err=val');
+        } else {
+          _showError(req, res, response.statusCode);
+        }
       }
-    }
-  );
+    );
+  }
 
 };
---
+++ public/javascripts/validation.js
@@ -0,0 +1,14 @@
+console.log('connected');
+$('#addReview').submit(function(e) {
+  var alertMessage = $('.alert.alert-danger')
+  alertMessage.hide();
+  console.log('activ e');
+  if (!$('input#name').val() || !$('select#rating').val() || !$('textarea#review').val()) {
+    if (alertMessage.length) {
+      alertMessage.show();
+    } else {
+      $(this).prepend('<div role="alert" class="alert alert-danger">All fields required, please try again</div>');
+    }
+    return false;
+  }
+});
