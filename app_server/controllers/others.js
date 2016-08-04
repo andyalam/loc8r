4e823e5e360cf8bebf816f0e32f0f112ca8691a9
Andy  Alameddine <andyalam@hotmail.com>
2016-08-04 10:37:21 -0700

    modularized angular app into its own folder and rerouted express

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,7 @@
 module.exports.about = function(req, res, next) {
     res.render('generic-text', { title: 'About' });
 }
+
+module.exports.angularApp = function(req, res, next) {
+    res.render('layout', { title: 'Loc8r' });
+};
