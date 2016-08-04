bb4ab1c3e6d19785174bd6cdb8c505d609641f3f
Andy  Alameddine <andyalam@hotmail.com>
2016-08-04 15:27:19 -0700

    iife wrappers to remove global scopes

--- app_client/common/filters/formatDistance.filter.js
+++ app_client/common/filters/formatDistance.filter.js
@@ -1,19 +1,21 @@
-angular
-  .module('loc8rApp')
-  .filter('formatDistance', formatDistance);
+(function() {
+  angular
+    .module('loc8rApp')
+    .filter('formatDistance', formatDistance);
 
 
-  var _isNumeric = function (number) {
-    return !isNaN(parseFloat(number)) && isFinite(number);
-  };
-
-  // Meters to Freedom units
-  function formatDistance() {
-    return function(distance) {
-      if (distance && _isNumeric(distance)) {
-        return (distance / 1609.34).toFixed(1) + ' miles';
-      } else {
-        return "?";
-      }
+    var _isNumeric = function (number) {
+      return !isNaN(parseFloat(number)) && isFinite(number);
     };
-  };
+
+    // Meters to Freedom units
+    function formatDistance() {
+      return function(distance) {
+        if (distance && _isNumeric(distance)) {
+          return (distance / 1609.34).toFixed(1) + ' miles';
+        } else {
+          return "?";
+        }
+      };
+    }
+})();
--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -1,14 +1,16 @@
-angular
-  .module('loc8rApp')
-  .service('loc8rData', loc8rData);
+(function() {
+  angular
+    .module('loc8rApp')
+    .service('loc8rData', loc8rData);
 
 
-function loc8rData ($http) {
-  var locationByCoords = function(lat, lng) {
-    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=10000');
-  };
+  function loc8rData ($http) {
+    var locationByCoords = function(lat, lng) {
+      return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=10000');
+    };
 
-  return {
-    locationByCoords: locationByCoords
-  };
-}
+    return {
+      locationByCoords: locationByCoords
+    };
+  }
+})();
