e3791cbfab344ae693f9d62958d843d9d63ea9f3
Andy  Alameddine <andyalam@hotmail.com>
2016-08-08 15:18:43 -0700

    login auth added to api

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -7,7 +7,7 @@ var sendJsonResponse = function(res, status, content) {
   res.json(content);
 };
 
-module.exports.register = function() {
+module.exports.register = function(req, res) {
   if(!req.body.name || !req.body.email || !req.body.password) {
     sendJsonResponse(res, 400, {
       "message": "All fields required."
@@ -30,10 +30,32 @@ module.exports.register = function() {
       });
     }
   });
-
 };
 
 
-module.exports.login = function() {
+module.exports.login = function(req, res) {
+  if(!req.body.email || !req.body.password) {
+    sendJsonResponse(res, 400, {
+      "message": "All fields required."
+    });
+    return;
+  }
 
+  passport.authenticate('local', function(err, user, info) {
+    var token;
+
+    if (err) {
+      sendJsonResponse(res, 404, err);
+      return;
+    }
+
+    if (user) {
+      token = user.generateJwt();
+      sendJsonResponse(res, 200, {
+        "token": token
+      });
+    } else {
+      sendJsonResponse(res, 401, info);
+    }
+  })(req, res);
 };
